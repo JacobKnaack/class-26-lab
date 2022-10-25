51b47018b37711fc2f3be837deaefec5138c63b7
Jacob Knaack <jacobknck@gmail.com>
2022-10-25 23:11:34 +0000

    Update 3 files

--- src/App.js
+++ src/App.js
@@ -9,49 +9,13 @@ import Footer from "./components/Footer/Footer";
 import Form from "./components/Form/Form";
 import Results from "./components/Results/Results";
 
-// class App extends React.Component {
-
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       data: null,
-//       requestParams: {}
-//     };
-//   }
-
-//   callApi = (requestParams) => {
-//     // mock output
-//     const data = {
-//       count: 2,
-//       results: [
-//         { name: "fake thing 1", url: "http://fakethings.com/1" },
-//         { name: "fake thing 2", url: "http://fakethings.com/2" }
-//       ]
-//     };
-//     this.setState({ data, requestParams });
-//   };
-
-//   render() {
-//     return (
-//       <React.Fragment>
-//         <Header />
-//         <div>Request Method: {this.state.requestParams.method}</div>
-//         <div>URL: {this.state.requestParams.url}</div>
-//         <Form handleApiCall={this.callApi} />
-//         <Results data={this.state.data} />
-//         <Footer />
-//       </React.Fragment>
-//     );
-//   }
-// }
-
 const App = () => {
   // state => the current value of our component State
   // setState => function that can update current state to a new state.
   const [state, setState] = useState({
     data: null,
     requestParams: {},
-    value: "Jacob"
+    value: "Jacob",
   });
 
   const callApi = (requestParams) => {
@@ -60,8 +24,8 @@ const App = () => {
       count: 2,
       results: [
         { name: "fake thing 1", url: "http://fakethings.com/1" },
-        { name: "fake thing 2", url: "http://fakethings.com/2" }
-      ]
+        { name: "fake thing 2", url: "http://fakethings.com/2" },
+      ],
     };
     setState({ data, requestParams, ...state });
   };
